441bd710c2f63328c4cfba2b9eca2438071d6fa3
RH0128 <tsumasaki27@gmail.com>
2025-03-12 19:49:30 +0900

    API連携

--- api/speech.js
+++ api/speech.js
@@ -1,19 +1,32 @@
-const handler = async (req, res) => {
+//1つ目の引数がフロントからデータを受け取る固有の場所になっている
+export default async function GET(request) {
+  //バックエンドのルート定義
+  console.log(request);
+
   try {
-    const { speaker, from, until } = req.query;
+    // URLSearchParamsを使って、検索用のクエリパラメータ（speaker、from、until）を設定
 
+    const params = new URLSearchParams({
+      speaker: request.query.speaker,
+      from: request.query.from,
+      until: request.query.until,
+    });
+
+    // console.log(3,params); もし上の処理が正しく行われているか確認したいなら、console.logを使って確認することができる
+
+    //APIにリクエストを送信
     const response = await fetch(
-      `https://kokkai.ndl.go.jp/api/speech?${new URLSearchParams({
-        speaker,
-        from,
-        until,
-        recordPacking: "json",
-      })}`
+      `https://kokkai.ndl.go.jp/api/speech?${params}`
     );
-
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    //APIから返ってきたデータを取得
     const data = await response.json();
-    res.json(data.speechRecord || []);
+    //フロントにデータを返す
+    return new Response(JSON.stringify(data));
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.error("Error fetching data:", error);
+    return new Response(JSON.stringify(error), { status: 500 });
   }
-};
+}
