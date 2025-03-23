7030e7c00fca423388f51b418184aceaa7392a9b
RH0128 <tsumasaki27@gmail.com>
2025-03-23 22:00:13 +0900

    slidebar

--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -192,7 +192,7 @@ function Sidebar({
       {/* This is what handles the sidebar gap on desktop */}
       <div
         className={cn(
-          "relative h-svh w-(--sidebar-width) bg-linear-[to_right,#fde8e4,#d6c9e5] transition-[width] duration-200 ease-linear", // 背景色をグラデーションに設定
+          "relative h-svh w-(--sidebar-width)  transition-[width] duration-200 ease-linear", // 背景色をグラデーションに設定
           "group-data-[collapsible=offcanvas]:w-0",
           "group-data-[side=right]:rotate-180",
           variant === "floating" || variant === "inset"
