42e3dcc21249a6baf97c2004c0073d4733fc11ad
RH0128 <79930122+RH0128@users.noreply.github.com>
2025-07-20 08:16:42 +0900

    サイドバーをなくす

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../App.css";
-import { AppSidebar } from "@/components/ui/app-sidebar";
 import { Separator } from "@/components/ui/separator";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
 import SearchForm from "@/components/ui/searchform";
-import { Button } from "@/components/ui/button";
 import Breadcrumbs from "@/components/ui/breadcrumbs";
 
 function App() {
@@ -29,31 +22,27 @@ function App() {
   const breadcrumbItems = []; // パンクズを空にする
 
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumbs items={breadcrumbItems} onHomeClick={goToHome} />
-          </div>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <SearchForm
-            speakerName={speakerName}
-            setSpeakerName={setSpeakerName}
-            meetingName={meetingName}
-            setMeetingName={setMeetingName}
-            dateRange={dateRange}
-            setDateRange={setDateRange}
-            isSearching={isSearching}
-            setIsSearching={setIsSearching}
-          />
-
+    <div>
+      <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear">
+        <div className="flex items-center gap-2 px-4">
+          {/* サイドバー関連の要素は削除 */}
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumbs items={breadcrumbItems} onHomeClick={goToHome} />
         </div>
-      </SidebarInset>
-    </SidebarProvider>
+      </header>
+      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <SearchForm
+          speakerName={speakerName}
+          setSpeakerName={setSpeakerName}
+          meetingName={meetingName}
+          setMeetingName={setMeetingName}
+          dateRange={dateRange}
+          setDateRange={setDateRange}
+          isSearching={isSearching}
+          setIsSearching={setIsSearching}
+        />
+      </div>
+    </div>
   );
 }
 
